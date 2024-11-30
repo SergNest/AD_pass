f40955e048b31a8dfb01dde181717318139d17bf
Serhii Nesterenko <sy.nesterenko@gmail.com>
2024-11-30 19:14:24 +0200

    some stylev2

--- static/scripts.js
+++ static/scripts.js
@@ -17,9 +17,24 @@ async function toggleStatus(samAccountName, action) {
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ sam_account_name: samAccountName, action: action })
     });
+
     const result = await response.json();
     alert(result.message);
+
     if (result.success) {
-        location.reload();
+        // Знайти відповідний рядок у таблиці за допомогою samAccountName
+        const row = document.querySelector(`tr[data-username="${samAccountName}"]`);
+
+        if (row) {
+            // Змінити клас рядка в залежності від статусу
+            if (action === 'disable') {
+                row.classList.add('disabled-user');  // Додаємо клас для "відключеного"
+                row.cells[2].innerText = "Відключений";  // Оновлюємо статус у таблиці
+            } else {
+                row.classList.remove('disabled-user');  // Видаляємо клас для "активного"
+                row.cells[2].innerText = "Активний";  // Оновлюємо статус у таблиці
+            }
+        }
     }
 }
+
